520b4b29844871984cbc0f4c610995d4c59bbe6f
Sebastian1468 <arizancasebastian14@gmail.com>
2026-07-09 12:08:48 -0500

    13

--- frontend/src/main.ts
+++
@@ -1,6 +0,0 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app'; // <-- Corregido aquí para que importe AppComponent
-
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
